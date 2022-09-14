c25bf1b16b276fa2963a8d5b9ea4a2edb241a7c0
Abdallahhany <abdallahhany253@gmail.com>
2022-09-14 03:15:38 +0200

    try to deploy frontend

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://trys3usingcli/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://trys3usingcli/
+aws s3 cp --recursive --acl public-read ./www s3://udagramprojectbucket/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagramprojectbucket/
