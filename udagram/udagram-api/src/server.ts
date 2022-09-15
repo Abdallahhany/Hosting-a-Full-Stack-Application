72e61744642b1e14a3cedaf8fcd2f32b22bcc574
Abdallah Hany <63977698+Abdallahhany@users.noreply.github.com>
2022-09-15 16:38:44 +0200

    Update server.ts

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -22,21 +22,21 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
+  app.use(cors());
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
-  app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
-    ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
-  }));
+//   app.use(cors({
+//     "allowedHeaders": [
+//       'Origin', 'X-Requested-With',
+//       'Content-Type', 'Accept',
+//       'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
+//       'Access-Control-Allow-Headers',
+//       'Access-Control-Allow-Methods'
+//     ],
+//     "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+//     "preflightContinue": true,
+//     "origin": '*',
+//   }));
 
   app.use("/api/v0/", IndexRouter);
 
